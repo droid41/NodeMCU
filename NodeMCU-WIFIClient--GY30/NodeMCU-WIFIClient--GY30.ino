5f54dff743d52ae422ed87f60f4be3752fe603f6
Alex Bloß <alex.bloss@gmx.de>
2016-09-09 00:09:07 +0200

    Transferring sensor data every 5 secs to thethings.io

---
+++ NodeMCU-WIFIClient--GY30/NodeMCU-WIFIClient--GY30.ino
@@ -0,0 +1,137 @@
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include "config.h"
+
+int ledPin = 3; // GPIO0
+int sdaPin = 4;
+int sclPin = 5;
+
+char host[] = "api.thethings.io";
+int httpPort = 80;
+
+int BH1750_address = 0x23; // i2c Addresse
+byte buff[2];
+
+ 
+void setup() {
+
+  Serial.begin(115200);
+  delay(10);
+
+  // I2C SDA 4, SCL 5
+  Wire.begin(sdaPin,sclPin);
+
+  // GY-30 Modul initialisieren
+  BH1750_Init(BH1750_address);
+
+  // On-Board-LED
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+ 
+void loop() {
+  
+  float lx = BH1750_ReadLX(BH1750_address);
+  if(lx < 0) {
+    Serial.print("> 65535");
+  }
+  else {
+    Serial.print((int)lx,DEC); 
+  }
+  Serial.println(" lx"); 
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  char str[50];
+  sprintf(str, "%d", (int)lx);
+  
+  String lxString = str;
+  String uri = "/v2/things/" + apiKey;
+  String data = "{ \"values\": [ { \"key\": \"lxval\", \"value\": " + lxString + " } ] }";
+
+  String postRequest = 
+    "POST " + uri + " HTTP/1.0\r\n" +
+    "Host: " + host + "\r\n" +
+    "Accept: application/json\r\n" +
+    "Content-Length: " + data.length() + "\r\n" +
+    "Content-Type: application/json\r\n" +
+    "\r\n" + data;
+
+  client.print(postRequest);
+               
+  delay(500);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+
+
+  delay(5000);
+}
+
+void BH1750_Init(int address){
+  
+  Wire.beginTransmission(address);
+  Wire.write(0x10); // 1 [lux] aufloesung
+  Wire.endTransmission();
+}
+
+byte BH1750_Read(int address){
+  
+  byte i=0;
+  Wire.beginTransmission(address);
+  Wire.requestFrom(address, 2);
+  while(Wire.available()){
+    buff[i] = Wire.read(); 
+    i++;
+  }
+  Wire.endTransmission();  
+  return i;
+}
+
+float BH1750_ReadLX(int address)
+{
+  float valf=0;
+  if(BH1750_Read(address)==2) {
+    valf=((buff[0]<<8)|buff[1])/1.2;
+    return valf;
+  }
+
+  return 0;
+}
+ 
